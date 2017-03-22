1bbf9aba2ee89b06951fc56d2fac6a4766e6057c
Nick <handnf@gmail.com>
2017-03-21 23:28:21 -0400

    Fixes bugs around use on a phone

--- app/src/main/java/news/androidtv/launchonboot/BootReceiver.java
+++ app/src/main/java/news/androidtv/launchonboot/BootReceiver.java
@@ -7,6 +7,7 @@ import android.content.IntentFilter;
 import android.media.tv.TvContract;
 import android.os.Build;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.felkertech.settingsmanager.SettingsManager;
 
@@ -26,10 +27,10 @@ public class BootReceiver extends BroadcastReceiver {
             Log.d(TAG, "Received intent");
             Log.d(TAG, intent.toString());
         }
-        if (!mScreenOnListener) {
+/*        if (!mScreenOnListener) {
             context.registerReceiver(this, new IntentFilter(Intent.ACTION_SCREEN_ON));
             mScreenOnListener = true;
-        }
+        }*/
 
         SettingsManager settingsManager = new SettingsManager(context);
         if (!settingsManager.getBoolean(SettingsManagerConstants.BOOT_APP_ENABLED)) {
@@ -52,8 +53,19 @@ public class BootReceiver extends BroadcastReceiver {
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(i);
         } else if (!settingsManager.getString(SettingsManagerConstants.LAUNCH_ACTIVITY).isEmpty()) {
-            Intent i = context.getPackageManager().getLeanbackLaunchIntentForPackage(
-                    settingsManager.getString(SettingsManagerConstants.LAUNCH_ACTIVITY));
+            Intent i;
+            if (context.getResources().getBoolean(R.bool.IS_TV)) {
+                i = context.getPackageManager().getLeanbackLaunchIntentForPackage(
+                        settingsManager.getString(SettingsManagerConstants.LAUNCH_ACTIVITY));
+            } else {
+                i = context.getPackageManager().getLaunchIntentForPackage(
+                        settingsManager.getString(SettingsManagerConstants.LAUNCH_ACTIVITY));
+            }
+
+            if (i == null) {
+                Toast.makeText(context, R.string.null_intent, Toast.LENGTH_SHORT).show();
+                return;
+            }
             i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(i);
         }
